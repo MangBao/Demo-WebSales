a5a89c437aa5b964b259fdbd78b9dc12e7fa3dfb
Mang Bảo - 60130041 <57796772+MangBao@users.noreply.github.com>
2020-06-29 00:02:57 +0700

    up

---
+++ assets/Js/myJS.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var modal_1 = document.getElementById('myModal1');
+    var modal_2 = document.getElementById('myModal2');
+
+    $('.sign-up').click(function (e) { 
+        e.preventDefault();
+        modal_1.style.display = 'block';
+    });
+
+    $('.sign-in').click(function (e) { 
+        e.preventDefault();
+        modal_2.style.display = 'block';
+    });
+
+    $('.back_1').click(function (e) { 
+        e.preventDefault();
+        modal_2.style.display = 'block';
+    });
+
+    $('.back_2').click(function (e) { 
+        e.preventDefault();
+        modal_1.style.display = 'block';
+    });
+
+    $('#myModal1').click(function (e) { 
+        e.preventDefault();
+        modal_1.style.display = 'none';
+    });
+
+    $('#myModal2').click(function (e) { 
+        e.preventDefault();
+        modal_2.style.display = 'none';
+    });
+});
